3d4a8f18bd1f5d7944954661621a50c502de7b45
temple7447 <starukido@gmail.com>
2023-08-31 12:23:00 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const app = express()
+
+
+
+
+app.get("/",(req,res)=>{
+    res.send("you are welcome")
+})
+
+const PORT = process.env.PORT || 4000
+
+
+
+app.listen(PORT,()=>{
+    console.log(`server is working ${PORT}`)
+})
